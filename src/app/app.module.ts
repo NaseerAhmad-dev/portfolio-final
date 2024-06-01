4c08e219a95c01d235f5a72813af47f10293b2bf
Naseer Ahmad <naseer.ahmad@purplemavens.com>
2024-06-01 23:33:57 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
